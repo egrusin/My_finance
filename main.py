1d2507d981c132ec7b705673f56554749aa3787b
Русинов Егор <rusinov-2001@inbox.ru>
2023-01-18 23:37:22 +0500

    add the main script 1.0 version

---
+++ main.py
@@ -0,0 +1,22 @@
+from excel_and_googlesheets import *
+from copy import copy
+
+
+SPREADSHEET_ID = '1kuAMznNIeI9zjzDT21RloxUSOexdYS50Mmpt2V-wP50'
+RANGE_NAME = 'finance!A:F'
+
+
+if __name__ == '__main__':
+    cur_workbook = Report('2023/1_Winter/January.xlsx')
+    tempsheet = cur_workbook.get_sheet('Транзакции')
+
+    day = get_today()
+    day_transactions = get_day_transactions(SPREADSHEET_ID, RANGE_NAME, day)
+
+    # print(day_transactions)
+    for tr in day_transactions:
+        cur_workbook.write_transaction(tempsheet, tr)
+    cur_workbook.save_book()
+
+
+
