8fe2b8be6791a3a6f53bc636713a8698f26f25f4
Русинов Егор <rusinov-2001@inbox.ru>
2023-01-24 23:36:28 +0500

    STABLE VERSION 1.3 (correct work with late dates)

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from excel_and_googlesheets import *
-from copy import copy
 
 # TO DO remember main and transaction sheet to most useful model
 # TO DO check and create directories for save Excel files
+# TO DO add minus for pay-transactions automatically
+# TO DO create a functions for main procedure
 
 SPREADSHEET_ID = '1kuAMznNIeI9zjzDT21RloxUSOexdYS50Mmpt2V-wP50'
 RANGE_NAME = 'finance!A:F'
@@ -11,15 +12,18 @@ RANGE_NAME = 'finance!A:F'
 if __name__ == '__main__':
     cur_workbook = Report('2023/1_Winter/January.xlsx')
     tempsheet = cur_workbook.get_sheet('Транзакции')
+    mainsheet = cur_workbook.get_sheet('Счета')
 
-    day = get_today()
-    day_transactions = get_day_transactions(SPREADSHEET_ID, RANGE_NAME, day)
+    today = get_today()
+    last_transaction_date = cur_workbook.get_last_report(tempsheet)
+    days = get_diff(last_transaction_date, today)
+
+    day_transactions = get_day_transactions(SPREADSHEET_ID, RANGE_NAME, days)
 
     for tr in day_transactions:
         cur_workbook.write_transaction(tempsheet, tr)
-    mainsheet = cur_workbook.get_sheet('Счета')
 
-    last_report = cur_workbook.get_last_report(mainsheet)
-    for date in get_diff(cur_workbook.get_last_report(mainsheet), day):
+    for date in get_diff(cur_workbook.get_last_report(mainsheet), today):
         cur_workbook.write_report(mainsheet, date)
+
     cur_workbook.save_book()
